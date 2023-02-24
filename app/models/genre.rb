684d4715048732c6e623213238a71cb2f79ce307
+ <+>
2023-02-24 22:37:35 +0300

    Assignment Done

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -4,13 +4,23 @@ class Genre < ActiveRecord::Base
 
   def song_count
     # return the number of songs in a genre
+    self.songs.count
   end
 
   def artist_count
     # return the number of artists associated with the genre
+    self.artists.count
   end
 
   def all_artist_names
     # return an array of strings containing every musician's name
+    songs.map { |song| song.artist.name }
+
+    # self.artists.pluck(:name)
+#     pluck is a method in Rails that allows you to retrieve a specific 
+#     column or attribute from a collection of ActiveRecord objects.
+
+# For example, if you have a collection of User objects and you only
+#  want to retrieve the name attribute of each user, you can use pluck like this:
   end
 end
